23264acb632916840108e668e4eea614bd71b198
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2019-07-18 15:16:42 +0200

    ajustements

--- src/templates/page-project.js
+++ src/templates/page-project.js
@@ -40,68 +40,69 @@ class PageProject extends Component {
                     template="template-project"
                     // pathname={location.pathname}
                 />
+                <div className="wrapper">
+                    <ul className="header">
+                        <li className="title">
+                            {titleFormated &&
+                                <h1 dangerouslySetInnerHTML={{
+                                    __html: titleFormated.childMarkdownRemark.html,
+                                }} />
+                            }
+                            {!titleFormated &&
+                                <h1>{title}</h1>
+                            }
+                        </li>
+                        <li className="metas">
+                            <div className="tags">{tags.join()}</div>
+                            <div className="date">{date}</div>
+                        </li>
+                        <li>
+                            <div
+                                className="texte"
+                                dangerouslySetInnerHTML={{
+                                    __html: texte.childMarkdownRemark.html,
+                                }}
+                            ></div>
+                        </li>
+                    </ul>
 
-                <ul className="header">
-                    <li className="title">
-                        {titleFormated &&
-                            <h1 dangerouslySetInnerHTML={{
-                                __html: titleFormated.childMarkdownRemark.html,
-                            }} />
-                        }
-                        {!titleFormated &&
-                            <h1>{title}</h1>
-                        }
-                    </li>
-                    <li className="metas">
-                        <div className="tags">{tags.join()}</div>
-                        <div className="date">{date}</div>
-                    </li>
-                    <li>
+                    <div className="images" style={{background: color}}>
+                        {images.map((image, key) => (
+                            <Img key={key} fluid={image.fluid} />
+                        ))}
+                    </div>
+                    
+                    {texteAlt &&
                         <div
-                            className="texte"
+                            className="texte-alt texte"
                             dangerouslySetInnerHTML={{
-                                __html: texte.childMarkdownRemark.html,
+                                __html: texteAlt.childMarkdownRemark.html,
                             }}
                         ></div>
-                    </li>
-                </ul>
-
-                <div className="images" style={{background: color}}>
-                    {images.map((image, key) => (
-                        <Img key={key} fluid={image.fluid} />
-                    ))}
-                </div>
-                
-                {texteAlt &&
-                    <div
-                        className="texte-alt texte"
-                        dangerouslySetInnerHTML={{
-                            __html: texteAlt.childMarkdownRemark.html,
-                        }}
-                    ></div>
-                }
-
-                <div className="footer">
-                    <ul className="pagination">
-                    {previous &&
-                        <li>
-                            <Link to={"project/"+previous.slug}>
-                                <div className="label">Previous project</div>
-                                <div className="pipe"></div>
-                                <div className="title">{previous.title}</div>
-                            </Link>
-                        </li>
                     }
-                    {next &&
-                        <li>
-                            <Link to={"project/"+next.slug}>
-                                <div className="title">{next.title}</div>
-                                <div className="pipe"></div>
-                                <div className="label">Next project</div>
-                            </Link>
-                        </li>
-                    }
-                    </ul>
+
+                    <div className="footer">
+                        <ul className="pagination">
+                        {previous &&
+                            <li>
+                                <Link to={"project/"+previous.slug}>
+                                    <div className="label">Previous project</div>
+                                    <div className="pipe"></div>
+                                    <div className="title">{previous.title}</div>
+                                </Link>
+                            </li>
+                        }
+                        {next &&
+                            <li>
+                                <Link to={"project/"+next.slug}>
+                                    <div className="title">{next.title}</div>
+                                    <div className="pipe"></div>
+                                    <div className="label">Next project</div>
+                                </Link>
+                            </li>
+                        }
+                        </ul>
+                    </div>
                 </div>
             </div>
         )
