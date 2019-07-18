691289f6efc4eb659c8f05a95a8277ab278d7cb9
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2019-07-18 11:01:40 +0200

    bg article

--- config/website.js
+++ config/website.js
@@ -12,8 +12,8 @@ module.exports = {
   favicon: 'src/images/icons/favicon.ico', // Used for manifest favicon generation
   shortName: 'violaineetjeremy', // shortname for manifest. MUST be shorter than 12 characters
   author: 'violaine et jeremy', // Author for schemaORGJSONLD
-  themeColor: '#0000ff',
-  backgroundColor: '#0000ff',
+  themeColor: '#F8E0D9',
+  backgroundColor: '#F8E0D9',
 
   instagram: '',
   twitter: '', // Twitter Username
