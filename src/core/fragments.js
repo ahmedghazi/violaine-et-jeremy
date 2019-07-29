7c867bbd1aeb18761ec2e7e75dfd37db52bc0c8e
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2019-07-29 16:20:36 +0200

    images

--- src/core/fragments.js
+++ src/core/fragments.js
@@ -56,9 +56,6 @@ export const query = graphql`
             ...sharpL
         }
         images {
-            file {
-                url
-            }
             ...sharpL
         }
     }
--- src/templates/page-project.js
+++ src/templates/page-project.js
@@ -40,8 +40,8 @@ class PageProject extends Component {
             next
         } = this.props.pageContext
 
-        const pageBanner = featuredImage ? featuredImage : images[0]
-        // console.log(pageBanner.file.url)
+        //const pageBanner = featuredImage ? featuredImage : images[0]
+        if(featuredImage)console.log(featuredImage)
         const tagList = tags ? tags : ""
 //console.log(pageBanner)
         // const _title = titleFormated 
@@ -52,9 +52,9 @@ class PageProject extends Component {
                 <SEO
                     pageTitle={title}
                     pageDescription={texte.childMarkdownRemark.excerpt}
-                    // pageBanner={pageBanner &&
-                    //     pageBanner.file.url
-                    // }
+                    pageBanner={featuredImage &&
+                        featuredImage.file.url
+                    }
                     page={true}
                     template="template-project"
                     // pathname={location.pathname}
