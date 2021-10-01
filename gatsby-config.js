53bda8c29abaee05276da72859176f6f68c3f46a
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2021-10-01 14:17:30 +0200

    push it t

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv")
 
-if (process.env.ENVIRONMENT !== 'production') {
-  dotenv.config();
+if (process.env.ENVIRONMENT !== "production") {
+  dotenv.config()
 }
-const { spaceId, accessToken } = process.env;
+const { spaceId, accessToken } = process.env
 
-const website = require('./config/website')
-const pathPrefix = website.pathPrefix === '/' ? '' : website.pathPrefix
+const website = require("./config/website")
+const pathPrefix = website.pathPrefix === "/" ? "" : website.pathPrefix
 
 module.exports = {
   siteMetadata: {
@@ -21,13 +21,13 @@ module.exports = {
     facebook: website.facebook,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-remark',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sass",
     {
-      resolve: 'gatsby-source-contentful',
+      resolve: "gatsby-source-contentful",
       options: {
         spaceId,
         accessToken,
@@ -39,7 +39,7 @@ module.exports = {
         component: require.resolve(`./src/components/layout.js`),
       },
     },
-    'gatsby-plugin-sitemap',
+    "gatsby-plugin-sitemap",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -49,15 +49,42 @@ module.exports = {
         start_url: pathPrefix,
         //background_color: website.backgroundColor,
         theme_color: website.themeColor,
-        display: 'standalone',
+        display: "standalone",
         icon: website.favicon,
       },
     },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: website.googleAnalyticsID,
+    //   },
+    // }
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: website.googleAnalyticsID,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "GA-288355952", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //     optimize_id: "OPT_CONTAINER_ID",
+        //     anonymize_ip: true,
+        //     cookie_expires: 0,
+        // },
+        // // This object is used for configuration specific to this plugin
+        // pluginConfig: {
+        //     // Puts tracking script in the head instead of the body
+        //     head: false,
+        //     // Setting this parameter is also optional
+        //     respectDNT: true,
+        //     // Avoids sending pageview hits from custom paths
+        //     exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // },
       },
-    }
+    },
   ],
-};
+}
