1c669662730aa616f0d58d4ce6f5ae8a19ff3bfe
ahmedghazi <atmet.ghazi@gmail.com>
2023-06-12 15:32:50 +0200

    landing ready

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,32 @@
-import "./globals.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import { PageContextProvider } from "./context/PageContext";
-import config from "./config/website";
-import "./styles/tailwind.css";
-import "./styles/index.scss";
+import "./globals.css"
+// import Footer from "./components/Footer"
+// import Header from "./components/Header"
+import { PageContextProvider } from "./context/PageContext"
+import config from "./config/website"
+import "./styles/tailwind.css"
+import "./styles/index.scss"
 
 export const metadata = {
   title: {
     template: `%s | ${config.title}`,
   },
   description: config.description,
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body>
-        {/* <Header /> */}
-        <div id='page'>
+        <div id="page">
           <PageContextProvider>
             <main>{children}</main>
           </PageContextProvider>
         </div>
-
-        {/* <Footer /> */}
-        {/* <Cursor /> */}
       </body>
     </html>
-  );
+  )
 }
