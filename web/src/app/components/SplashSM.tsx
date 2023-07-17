d67096c2cb09a322ad7ad6fc04bd501bef496020
ahmedghazi <atmet.ghazi@gmail.com>
2023-07-17 18:35:26 +0200

    build it

--- web/src/app/components/SplashSM.tsx
+++ web/src/app/components/SplashSM.tsx
@@ -11,26 +11,29 @@ const Container = styled.div`
 `
 
 const SplashSM = (props: Props) => {
-  // const [isEnd, setIsEnd] = useState<boolean>(false)
-  const [scope, animate] = useAnimate()
+  const [isEnd, setIsEnd] = useState<boolean>(false)
+  // const [scope, animate] = useAnimate()
   // useEffect(() => {}, [])
 
   const duration: number = 0.6
   const violaine: string[] = "IOLAINE".split("")
   const jeremy: string[] = "ÉRÉMY".split("")
 
-  // const containeVariants = {
-  //   start: {
-  //     opacity: 1,
-  //   },
-  //   end: {
-  //     opacity: 0,
-
-  //     transitionEnd: {
-  //       display: "none",
-  //     },
-  //   },
-  // }
+  const containeVariants = {
+    start: {
+      opacity: 1,
+    },
+    end: {
+      opacity: 0,
+      transition: {
+        duration: duration,
+        delay: 0.5,
+      },
+      transitionEnd: {
+        display: "none",
+      },
+    },
+  }
   const letterVariants = {
     anime: {
       width: 0,
@@ -50,22 +53,13 @@ const SplashSM = (props: Props) => {
   const _onAnimationComplete = (item: string) => {
     if (item === "Y") {
       console.log("_onAnimationComplete", item)
-      // setIsEnd(true)
-      animate(
-        scope.current,
-        {
-          opacity: 0,
-          transitionEnd: {
-            display: "none",
-          },
-        },
-        { duration: 1, delay: 1 }
-      )
+      setIsEnd(true)
     }
   }
   return (
-    <div
-      ref={scope}
+    <motion.div
+      variants={containeVariants}
+      animate={isEnd && "end"}
       className="splash splash--sm fixed inset-0 bg-bg z-50 text-lg"
     >
       <div className="center-x-y outter h-full">
@@ -107,7 +101,7 @@ const SplashSM = (props: Props) => {
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
