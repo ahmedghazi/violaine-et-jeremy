5b7d00b7fb26ca7769643f58bd8cd6ff60aa5724
ahmedghazi <atmet.ghazi@gmail.com>
2023-07-19 11:55:37 +0200

    build it

--- web/src/app/components/workUI/index.tsx
+++ web/src/app/components/workUI/index.tsx
@@ -13,7 +13,7 @@ type Props = {
 }
 
 const WorkUI = ({ worksImages, worksTexts, isDesign }: Props) => {
-  const [display, setDisplay] = useState<string>("list")
+  const [display, setDisplay] = useState<string>("grid")
   // console.log(allWorks)
   return (
     <div className="work-ui">
