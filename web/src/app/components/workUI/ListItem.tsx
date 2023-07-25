d3b53713cffbe9f17646404a26ee0a203c886638
ahmedghazi <atmet.ghazi@gmail.com>
2023-07-25 11:39:49 +0200

    build it

--- web/src/app/components/workUI/ListItem.tsx
+++ web/src/app/components/workUI/ListItem.tsx
@@ -88,7 +88,40 @@ const ListItem = ({ input, hasLink }: Props) => {
             className="btn-toggle-images sm-only"
             onClick={() => setActive(!active)}
           >
-            {active ? "-" : "✛"}
+            {active ? (
+              <svg
+                version="1.1"
+                id="Calque_1"
+                xmlns="http://www.w3.org/2000/svg"
+                // xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                width="15px"
+                viewBox="0 0 340.9 53"
+                // style="enable-background:new 0 0 340.9 53;"
+                // xml:space="preserve"
+              >
+                <rect width="142.1" height="53" />
+                <rect x="198.8" width="142.1" height="53" />
+              </svg>
+            ) : (
+              <svg
+                version="1.1"
+                id="Calque_1"
+                xmlns="http://www.w3.org/2000/svg"
+                // xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                width="15px"
+                viewBox="0 0 340.9 337.3"
+                // xml:space="preserve"
+              >
+                <path
+                  d="M0,142.1h142.1v53H0V142.1z M198.8,0v142.1h-56.6V0H198.8z M142.1,337.3V195.2h56.6v142.1H142.1z M340.9,142.1v53H198.8
+		v-53H340.9z"
+                />
+              </svg>
+            )}
           </button>
           {hasLink && active && (
             <Link href={hasLink ? _linkResolver(input) : "#"}>
